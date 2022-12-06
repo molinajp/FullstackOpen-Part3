60660a28a05dca2b014cf710b3ff04a95a3eda2f
Juan Molina <juan.molina@globallogic.com>
2022-12-06 18:02:23 -0300

    deployed in fly.io

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
@@ -23,7 +24,9 @@ app.use(morgan(function (tokens, req, res) {
 }))
 
 //adds a '-' when body is empty
-//app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
+
+app.use(cors())
 
 let persons = [
     {
@@ -86,7 +89,7 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has ${persons.length} entries </p>  ${new Date()}`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || "8080";
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
