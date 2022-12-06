23f184cdbcc655914eaa9087685e177342d1a04f
Juan Molina <juan.molina@globallogic.com>
2022-12-06 18:23:00 -0300

    server and front deployed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Link to app deployed in Fly.io
 
-https://phonebook-server-jp.fly.dev/ (morgan code is commented in the deployed version)
+https://phonebook-server-jp.fly.dev/ 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', function getBody(req) {
     if (req.body && Object.keys(req.body).length !== 0) {
