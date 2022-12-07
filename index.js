a6c71179c6ca17f0ec62c644bfa10a7643f5e502
Juan Molina <juan.molina@globallogic.com>
2022-12-07 17:43:09 -0300

    eslint run in project

--- index.js
+++ index.js
@@ -9,144 +9,115 @@ app.use(express.json())
 app.use(express.static('build'))
 
 morgan.token('body', function getBody(req) {
-    if (req.body && Object.keys(req.body).length !== 0) {
-        return JSON.stringify(req.body)
-    }
+  if (req.body && Object.keys(req.body).length !== 0) {
+    return JSON.stringify(req.body)
+  }
 })
 
 //works the way I expected
 app.use(morgan(function (tokens, req, res) {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        tokens['body'](req, res)
-    ].join(' ')
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['body'](req, res)
+  ].join(' ')
 }))
 
 //adds a '-' when body is empty
-app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
+//app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
 app.use(cors())
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
-    
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).send(`There's no person with id ${request.params.id}`).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).send(`There's no person with id ${request.params.id}`).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person
-        .findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      console.log(result)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-    const person = request.body
-    if (!person.number || !person.name) {
-        response.status(400).json({ 'error': `fields can't be blank` })
-    }/* else if (persons.filter(p => p.name === person.name).length !== 0) {
-        response.status(400).json({ 'error': `name must be unique` })
-    } else {
-        person.id = Math.floor(Math.random() * 100000000)
-        persons = persons.concat(person)
-        response.json(person)
-    }*/
-    const personToBeSaved = new Person({
-        name: person.name,
-        number: person.number
-    })
-    personToBeSaved.save().then(person => {
-        response.status(201).json(person)
-    })
+app.post('/api/persons', (request, response, next) => {
+  const person = request.body
+  const personToBeSaved = new Person({
+    name: person.name,
+    number: person.number
+  })
+  personToBeSaved.save().then(person => {
+    response.status(201).json(person)
+  })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const { name, number } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-
-    if (!person.number || !person.name) {
-        response.status(400).json({ 'error': `fields can't be blank` }).end()
-    } else{
-        Person
-        .findByIdAndUpdate(request.params.id, person, {new: true})
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
-    }
+  Person
+    .findByIdAndUpdate(
+      request.params.id,
+      { name, number },
+      { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has ${persons.length} entries </p>  ${new Date()}`)
+  Person
+    .count()
+    .then(number => {
+      response.send(`<p>Phonebook has ${number} entries </p>  ${new Date()}`)
+    })
 })
 
-const PORT = process.env.PORT || "8080";
+const PORT = process.env.PORT || '8080'
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'MongoServerError') {
+    return response.status(400).json({ error: error.message })
   }
-  
-  // this has to be the last loaded middleware.
-  app.use(errorHandler)
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -6,6 +6,7 @@ const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
   .then(result => {
+    console.log(result)
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -13,16 +14,36 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^[0-9]{2,3}-[0-9]{7,8}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+personSchema.on('index', function (err) { // <-- Wait for model's indexes to finish
+  // eslint-disable-next-line no-undef
+  assert.ifError(err)
+})
 
 module.exports = mongoose.model('Person', personSchema)
